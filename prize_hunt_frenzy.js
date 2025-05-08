c5117078ee80f469bb71d1b3bbe855ec315c8a43
Andy Agyei-Dwumah <andydwumah@gmail.com>
2025-05-08 23:42:01 +0100

    Improvements to code quality
    - Add fieldset tag to surround radio buttons
    - Remove media queries
    - Update colours for prize and traps
    - Add reset layer
    - Add sizeOfBoard variable to ensure that the board size can be updated easily

--- prize_hunt_frenzy.js
+++ prize_hunt_frenzy.js
@@ -5,10 +5,11 @@ const getRandomTimeInterval = () => {
     return Math.floor(Math.random()*(max-min+1) + min);
 }
 
-// Selecting a random table cell (table is 20x20 = 400)
+// Selecting a random table cell (e.g., if 'sizeOfBoard' is 20 then 20x20 = 400 - 1 = 399)
+// Must be 1 less to prevent indexError
 const getRandomCellIndex = () => {
     let min = Math.ceil(0);
-    let max = Math.floor(399);
+    let max = Math.floor((sizeOfBoard * sizeOfBoard) - 1);
     return Math.floor(Math.random()*(max-min+1) + min);
 }
 
@@ -41,7 +42,7 @@ const getDifficultylevelTimes = difficultyLevel => {
 /////////////////////////
 const scoreParaElement = document.querySelector('.score');
 const timeParaElement = document.querySelector('.time');
-const main = document.getElementById("main");
+const main = document.querySelector("main");
 // The higher the number, the more prizes and traps will be on the board
 const frequencyOfPrizesAndTraps = 200;
 const numOfSecondsGameLasts = 30;
@@ -51,6 +52,7 @@ const decreaseScoreNum = -100;
 const endGameNum = 0;
 const scoreText = "Score:";
 const timeRemainingText = "Time remaining:";
+const sizeOfBoard = 15;  // Adjust to suit your needs!
 
 let gameFinished = false;
 let counter;  // Declare the timer
@@ -58,20 +60,20 @@ let score = initialScore;
 let timer = numOfSecondsGameLasts;
 
 /////////////////////////
-const myDiv = document.getElementById("game-board-div");  // Create table
+const gameBoard = document.getElementById("game-board-div");  // Create table
 const table = document.createElement('table');
 table.className = "mx-auto";
-myDiv.appendChild(table);
+gameBoard.appendChild(table);
 
 const tbody = document.createElement('tbody');  // Create table body
 table.appendChild(tbody);
 
-// Create 20 table rows and inside each table row, create 20 table cells
-for (i = 0; i < 20; i++) {
-    let tableRow = document.createElement('tr'); 
+// Create 'sizeOfBoard' table rows and inside each table row, create 'sizeOfBoard' table cells
+for (i = 0; i < sizeOfBoard; i++) {
+    const tableRow = document.createElement('tr'); 
     tbody.appendChild(tableRow);
-    for (let x = 0; x < 20; x++) {
-            let tableCell = document.createElement('td');
+    for (let x = 0; x < sizeOfBoard; x++) {
+            const tableCell = document.createElement('td');
             tableRow.appendChild(tableCell);
             tableCell.className = "cell";
         }
@@ -96,19 +98,19 @@ const endGame = () => {
     gameFinished = true;
 
     // Create 'game over' row and text
-    let buttonContainer = document.createElement("div");
+    const buttonContainer = document.createElement("div");
     buttonContainer.className = "pt-3";
-    let button = document.createElement("button");
+    const button = document.createElement("button");
     button.setAttribute("type", "button");
     button.setAttribute("data-bs-toggle", "modal");
     button.setAttribute("data-bs-target", "#myModal");
-    button.className = "btn btn-primary modalPlayAgainButton py-2 px-3 fs-5";
+    button.className = "btn btn-primary py-2 px-3 fs-5";
     button.innerHTML = "Play Again?";
-    let row = document.createElement("div");
+    const row = document.createElement("div");
     row.className = "row game-over-row";
-    let column = document.createElement("div");
+    const column = document.createElement("div");
     column.className = "col-md-5 mx-auto";
-    let gameOverContainer = document.createElement("div");
+    const gameOverContainer = document.createElement("div");
     gameOverContainer.className = "p-3";
     gameOverContainer.innerHTML = `Game over. Your score is ${score}`;
     gameOverContainer.style.fontWeight = "700";
@@ -139,11 +141,12 @@ const countdown = () => {  // Timer
 const generatePrizeOrATrapCell = () => {
   let difficultyLevel = document.forms[0].elements.difficulty.value;
   let difficultyLevelTimes = getDifficultylevelTimes(difficultyLevel);
+  const percentageToBecomeAPrize = 0.6;
 
-  if (Math.random() <= 0.6) {
+  if (Math.random() <= percentageToBecomeAPrize) {
     // 60% chance to become a prize
     // Select a random table cell on the board
-    let prizeCell = document.getElementsByTagName("td")[getRandomCellIndex()];
+    const prizeCell = document.getElementsByTagName("td")[getRandomCellIndex()];
     prizeCell.className = "prize"; // Make it a prize
     prizeCell.setAttribute("onclick", "increaseOrDecreaseScore(this)");
     setTimeout(() => {
@@ -153,7 +156,7 @@ const generatePrizeOrATrapCell = () => {
       prizeCell.classList.add("cell");
     }, getRandomDuration(difficultyLevelTimes[0], difficultyLevelTimes[1]));
   } else {
-    let trapCell = document.getElementsByTagName("td")[getRandomCellIndex()];
+    const trapCell = document.getElementsByTagName("td")[getRandomCellIndex()];
     trapCell.className = "trap";
     trapCell.setAttribute("onclick", "increaseOrDecreaseScore(this)");
     setTimeout(() => {
@@ -185,7 +188,7 @@ const startGame = () => {
 }
 
 const restartGame = () => {
-    let gameOverRow = document.querySelector(".game-over-row");
+    const gameOverRow = document.querySelector(".game-over-row");
     main.removeChild(gameOverRow);  // Remove game over row, including its text
     gameFinished = false;
     score = initialScore;
@@ -193,11 +196,12 @@ const restartGame = () => {
 }
 
 /////////////////////////
-const modalForm = document.querySelector(".modal-form");
+const modalForm = document.getElementById("modal-form");
 
 modalForm.addEventListener("submit", e => {
-    let difficultyLevel = document.forms[0].elements.difficulty.value;
+    e.preventDefault();
 
+    const difficultyLevel = document.forms[0].elements.difficulty.value;
     if (!difficultyLevel) {  // Check if a difficult level was chosen by the user
         return alert("Please enter a difficulty level");
     }
@@ -205,12 +209,11 @@ modalForm.addEventListener("submit", e => {
         restartGame();
     }
 
-    let modalDivButton = document.querySelector(".modal-div-button");
+    const modalDivButton = document.querySelector(".modal-start-button");
     // Remove start button after user submits form the first time
     if (modalDivButton.style.display !== "none") {
         modalDivButton.style.display = "none";
         main.style.display = "block";
     }
-    e.preventDefault();
     startGame();
 });
